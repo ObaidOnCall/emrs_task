c04a177e049a9e44d3d83c3f577ad12f9ee4f169
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2025-01-17 00:37:50 +0100

    Add DepartmentService

---
+++ src/main/java/io/hahn_software/emrs/services/DepartmentService.java
@@ -0,0 +1,13 @@
+package io.hahn_software.emrs.services;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Transactional
+public class DepartmentService {
+    
+}
