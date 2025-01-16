5e0418f0074c442bf78aff87c78e747468512575
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2025-01-17 00:38:19 +0100

    Start Integration Tests

---
+++ src/test/java/io/hahn_software/emrs/integration/BaseControllerTest.java
@@ -0,0 +1,10 @@
+package io.hahn_software.emrs.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@Slf4j
+public abstract class BaseControllerTest {
+}
