7aae845b08a21e104b712871bccf8a415b7fe4e9
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2025-01-19 00:49:20 +0100

    Help Spring configuration discover Env Vars For hibernate setup

--- src/main/java/io/hahn_software/emrs/conf/DbConf.java
+++ src/main/java/io/hahn_software/emrs/conf/DbConf.java
@@ -2,9 +2,11 @@ package io.hahn_software.emrs.conf;
 
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -17,11 +19,21 @@ import jakarta.persistence.EntityManagerFactory;
 @Configuration
 @EnableTransactionManagement
 public class DbConf {
+
+
+    @Autowired
+    private Environment env;
     
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(@Qualifier("propertiesLoader1") PropertiesLoader propertiesLoader) {
+
         Properties properties = propertiesLoader.loadProperties("src/main/resources/application.properties");
+
+        properties.put("hibernate.connection.url", resolveJdbcUrl());
+        properties.put("hibernate.connection.password", env.getProperty("DB_PASSWORD" , "incorrect_password")); 
+        properties.put("hibernate.connection.username", env.getProperty("DB_USER" , "keycloak_user"));
+        properties.put("hibernate.connection.driver_class", "org.postgresql.Driver");
 
         LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
 
@@ -51,5 +63,15 @@ public class DbConf {
         jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
 
         return jpaTransactionManager ;
+    }
+
+
+
+    private String resolveJdbcUrl() {
+        String urlTemplate = env.getProperty("jdbc.url.template");
+        return urlTemplate
+                .replace("${DB_HOST}", env.getProperty("DB_HOST"))
+                .replace("${DB_NAME}", env.getProperty("DB_NAME"))
+                .replace("${DB_SCHEMA}", env.getProperty("DB_SCHEMA"));
     }
 }
