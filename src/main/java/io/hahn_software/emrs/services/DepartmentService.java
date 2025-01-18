0e621d5ba191cef9f94ed1fe85bb476cd9db16ca
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2025-01-18 15:11:55 +0100

    Add Departement Endpoints

--- src/main/java/io/hahn_software/emrs/services/DepartmentService.java
+++ src/main/java/io/hahn_software/emrs/services/DepartmentService.java
@@ -1,13 +1,17 @@
 package io.hahn_software.emrs.services;
 
+import java.util.Collections;
 import java.util.List;
 
+import org.hibernate.query.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.hahn_software.emrs.dao.repositories.DepartmentRepo;
 import io.hahn_software.emrs.dtos.DepartmentRequest;
 import io.hahn_software.emrs.dtos.DepartmentResponse;
+import io.hahn_software.emrs.dtos.OperationResult;
+import io.hahn_software.emrs.dtos.PageDTO;
 import io.hahn_software.emrs.entities.Department;
 import io.hahn_software.emrs.mappers.DepartmentMapper;
 import jakarta.transaction.Transactional;
@@ -34,6 +38,12 @@ public class DepartmentService {
     }
 
 
+    /**
+     * Creates multiple departments in batch.
+     *
+     * @param departmentRequests List of DepartmentRequest objects.
+     * @return List of DepartmentResponse objects for the created departments.
+     */
 
 
 
@@ -45,5 +55,72 @@ public class DepartmentService {
         List<Department> savedDepartments = departmentRepo.insertDepartmentInBatch(departments);
 
         return departmentMapper.toDepartmentResponseList(savedDepartments);
+    }
+
+
+    /**
+     * Deletes departments by their IDs.
+     *
+     * @param departmentIds List of department IDs to delete.
+     * @return Number of departments deleted.
+     */
+
+    public OperationResult deleteDepartments(List<Long> departmentIds) {
+        log.info("Deleting departments with IDs: {}", departmentIds);
+
+        if (departmentIds == null || departmentIds.isEmpty()) {
+            log.warn("No department IDs provided for deletion");
+            return OperationResult.of(0, "No department IDs provided.");
+        }
+
+        int deletedCount = departmentRepo.deleteDepartments(departmentIds);
+
+        return OperationResult.of(deletedCount , "Delete operation completed successfully.");
+    }
+
+
+
+    /**
+     * Finds departments by their IDs.
+     *
+     * @param departmentIds List of department IDs to find.
+     * @return List of DepartmentResponse objects for the found departments.
+     */
+    public List<DepartmentResponse> findDepartments(List<Long> departmentIds) {
+        log.info("Finding departments with IDs: {}", departmentIds);
+
+        if (departmentIds == null || departmentIds.isEmpty()) {
+            log.warn("No department IDs provided for search");
+            return Collections.emptyList();
+        }
+
+        List<Department> departments = departmentRepo.findDepartments(departmentIds);
+        return departmentMapper.toDepartmentResponseList(departments);
+    }
+
+
+
+    /**
+     * Retrieves departments with pagination.
+     *
+     * @param page     Page number (starting from 0).
+     * @param pageSize Number of departments per page.
+     * @return List of DepartmentResponse objects for the retrieved departments.
+     */
+    public PageDTO<DepartmentResponse> getDepartmentsWithPagination(int page, int pageSize) {
+        
+
+        log.info("Retrieving departments with pagination - Page: {}, PageSize: {}", page, pageSize);
+
+        List<Department> departments = departmentRepo.departmentsWithPagination(page, pageSize);
+
+        List<DepartmentResponse> content = departmentMapper.toDepartmentResponseList(departments);
+
+        long totalElements = departmentRepo.count();
+
+        int totalPages = (int) Math.ceil((double) totalElements / pageSize);
+
+        return new PageDTO<>(content, page, pageSize, totalElements, totalPages);
+
     }
 }
--- src/main/java/io/hahn_software/emrs/web/DepartmentController.java
+++ src/main/java/io/hahn_software/emrs/web/DepartmentController.java
@@ -1,23 +1,35 @@
 package io.hahn_software.emrs.web;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.hahn_software.emrs.dtos.DepartmentRequest;
 import io.hahn_software.emrs.dtos.DepartmentResponse;
+import io.hahn_software.emrs.dtos.OperationResult;
+import io.hahn_software.emrs.dtos.PageDTO;
 import io.hahn_software.emrs.services.DepartmentService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -38,6 +50,7 @@ public class DepartmentController {
 
 
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_HR_Personnel') or hasRole('ROLE_Administrator')")
     @Operation(
         summary = "Create departments in batch",
         description = "Creates multiple departments in a single batch operation."
@@ -65,5 +78,125 @@ public class DepartmentController {
         List<DepartmentResponse> responses = departmentService.createDepartments(departmentRequests);
 
         return new ResponseEntity<>(responses, HttpStatus.CREATED);
+    }
+
+
+
+
+
+
+    @DeleteMapping("{departmentIds}")
+    @PreAuthorize("hasRole('ROLE_HR_Personnel') or hasRole('ROLE_Administrator')")
+    @Operation(
+        summary = "Delete departments by IDs",
+        description = "Deletes departments with the provided IDs."
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "Departments deleted successfully",
+        content = @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = OperationResult.class)
+        )
+    )
+    @ApiResponse(
+        responseCode = "400",
+        description = "Invalid input provided",
+        content = @Content(schema = @Schema(hidden = true))
+    )
+    @ApiResponse(
+        responseCode = "500",
+        description = "Internal server error",
+        content = @Content(schema = @Schema(hidden = true))
+    )
+    public ResponseEntity<OperationResult> deleteDepartments(
+
+        @Parameter(
+            description = "Comma-separated list of department Ids to be deleted",
+            required = true,
+            example = "19887,18995",
+            schema = @Schema(type = "string")
+        )
+        @PathVariable List<Long> departmentIds
+    ) {
+        OperationResult result = departmentService.deleteDepartments(departmentIds);
+        return ResponseEntity.ok(result);
+    }
+
+
+
+    @GetMapping("/{departmentIds}")
+    @PreAuthorize("hasRole('ROLE_HR_Personnel') or hasRole('ROLE_Administrator') or (hasRole('ROLE_Manager') and @departmentSecurityService.isManagerOfDepartment(#departmentId))")
+    @Operation(
+        summary = "Find departments by IDs",
+        description = "Finds departments with the provided IDs."
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "Departments found successfully",
+        content = @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = DepartmentResponse.class, type = "array")
+        )
+    )
+    @ApiResponse(
+        responseCode = "400",
+        description = "Invalid input provided",
+        content = @Content(schema = @Schema(hidden = true))
+    )
+    @ApiResponse(
+        responseCode = "500",
+        description = "Internal server error",
+        content = @Content(schema = @Schema(hidden = true))
+    )
+    public ResponseEntity<List<DepartmentResponse>> findDepartments(
+
+        @Parameter(
+            description = "Comma-separated list of department IDs to be fetech",
+            required = true,
+            example = "1,2,3",
+            schema = @Schema(type = "string")
+        )
+        @PathVariable List<Long> departmentIds
+
+    ) {
+        List<DepartmentResponse> departments = departmentService.findDepartments(departmentIds);
+        return ResponseEntity.ok(departments);
+    }
+
+
+
+
+    @GetMapping
+    @PreAuthorize("hasRole('ROLE_HR_Personnel') or hasRole('ROLE_Administrator') or (hasRole('ROLE_Manager') and @departmentSecurityService.isManagerOfDepartment(#departmentId))")
+    @Operation(
+        summary = "Get departments with pagination",
+        description = "Retrieves departments with pagination."
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "Departments retrieved successfully",
+        content = @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = DepartmentResponse.class, type = "array")
+        )
+    )
+    @ApiResponse(
+        responseCode = "400",
+        description = "Invalid input provided",
+        content = @Content(schema = @Schema(hidden = true))
+    )
+    @ApiResponse(
+        responseCode = "500",
+        description = "Internal server error",
+        content = @Content(schema = @Schema(hidden = true))
+    )
+    public ResponseEntity<PageDTO<DepartmentResponse>> getDepartmentsWithPagination(
+        @RequestParam int page,
+        @RequestParam int pageSize
+    ) {
+        return ResponseEntity.ok(
+            departmentService.getDepartmentsWithPagination(page, pageSize)
+        );
     }
 }
